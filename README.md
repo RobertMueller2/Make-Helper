6c12de94fd3437a3522a57c2a2b04daedd7b6b00
Rene D. Obermueller <cmdrrdo@gmail.com>
2023-04-07 10:42:48 +0200

    Add helper and adjust README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Make-Helper
+Make-Helper
+==
+
+Contains a few things to reduce redundant stuff in my Makefiles.
+
+Usage
+--
+
+```make
+# make sure something like this is placed before the include to retain the default recipe
+all: build
+
+include path/to/Make-Helper/make-helper.inc.mk
+
+```
---
+++ make-helper.inc.mk
@@ -0,0 +1,18 @@
+# include after default target, e.g. all:
+
+OLD_VARS:= $(filter-out NEWTARGET,$(.VARIABLES))
+
+.PHONY: vars targets
+
+vars:
+	$(foreach v,$(filter-out $(OLD_VARS) OLD_VARS,$(.VARIABLES)), $(info $(v) = $($(v))))
+
+#FIXME: this only uses the main Makefile
+targets:
+	@grep -Po '^[^\.#[:space:]].*(?=:(?!=))' $(CURDIR)/$(firstword $(MAKEFILE_LIST))
+
+# e.g. to be able to use $\{n} in $(subst
+define \n
+
+
+endef
