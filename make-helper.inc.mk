1fe3d5d29fa6e85201579104fd2ef5883f7c8086
Rene D. Obermueller <cmdrrdo@gmail.com>
2023-11-26 08:15:01 +0100

    add better targets recipe

--- make-helper.inc.mk
+++ make-helper.inc.mk
@@ -7,9 +7,9 @@ OLD_VARS:= $(filter-out NEWTARGET,$(.VARIABLES))
 vars:
 	$(foreach v,$(filter-out $(OLD_VARS) OLD_VARS,$(.VARIABLES)), $(info $(v) = $($(v))))
 
-#FIXME: this only uses the main Makefile
+# see https://stackoverflow.com/questions/4219255/how-do-you-get-the-list-of-targets-in-a-makefile
 targets:
-	@grep -Po '^[^\.#[:space:]].*(?=:(?!=))' $(CURDIR)/$(firstword $(MAKEFILE_LIST))
+	@LC_ALL=C $(MAKE) -pRrq -f $(firstword $(MAKEFILE_LIST)) : 2>/dev/null | awk -v RS= -F: '/(^|\n)# Files(\n|$$)/,/(^|\n)# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | sort | grep -E -v -e '^[^[:alnum:]]' -e '^$@$$'
 
 # e.g. to be able to use $\{n} in $(subst
 define \n
